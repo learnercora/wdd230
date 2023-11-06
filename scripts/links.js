831c97e003685f04d349cb569e7bf3ec53c1e616
coralin <zxc188950@yahoo.com.tw>
2023-11-06 10:31:30 +0800

    w09

--- scripts/links.js
+++ scripts/links.js
@@ -1,4 +1,33 @@
 const baseURL = "https://learnercora.github.io/wdd230/";
 const linksURL = "https://learnercora.github.io/wdd230/data/links.json";
 
-console.log(linksURL);
+const menu = document.querySelector('section ul');
+// console.log(menu);
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    // console.log(data);
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach( (week) => {
+        let ulE = document.createElement("ul");
+        let liE = document.createElement("li");
+        let aE = document.createElement("a");
+
+        liE.innerHTML = `${week.lesson}`;
+        console.log(week)
+        // aE.setAttribute("href", "123");
+        // aE.setAttribute("target", "_blank");
+
+
+        ulE.appendChild(liE);
+        ulE.appendChild(aE);
+
+        menu.appendChild(ulE);
+    });
+}
+
+getLinks();
