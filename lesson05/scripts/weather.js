a52b801ebe7c38be0fa666ec268f1f67960ec17c
coralin <zxc188950@yahoo.com.tw>
2023-11-10 14:05:02 +0800

    Consuming a Weather API

---
+++ lesson05/scripts/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=a688831c2a7f3e05d05b5fc3c28b7210";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            // console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) { 
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `weather icon - ${data.weather[0].icon}`)
+    captionDesc.textContent = desc.toUpperCase();
+}
+
+apiFetch();
