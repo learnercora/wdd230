322d21d3614ff9fbecea0ea87b0e9908be5f4fba
coralin <zxc188950@yahoo.com.tw>
2023-11-21 11:22:26 +0800

    fix

--- chamber/scripts/mode-switch.js
+++ chamber/scripts/mode-switch.js
@@ -2,7 +2,7 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 const checkbox = document.querySelector('input[type="checkbox"]');
 const card = document.querySelectorAll(".card");
-const h3 = document.querySelectorAll("h3");
+const headings = document.querySelectorAll("h2, h3, h4");
 
 
 if (checkbox.checked) {
@@ -12,9 +12,9 @@ if (checkbox.checked) {
 		cardElement.classList.remove('light-mode-for-card');
 		cardElement.classList.add('dark-mode-for-card');
 	});
-	h3.forEach((h3Element) => {
-		h3Element.classList.remove('light-mode-for-h3');
-		h3Element.classList.add('dark-mode-for-h3');
+	headings.forEach((h3Element) => {
+		h3Element.classList.remove('light-mode-for-heading');
+		h3Element.classList.add('dark-mode-for-heading');
 	});
 } else {
 	main.classList.remove('dark-mode-for-main');
@@ -23,9 +23,9 @@ if (checkbox.checked) {
 		cardElement.classList.remove('dark-mode-for-card');
 		cardElement.classList.add('light-mode-for-card');
 	});
-	h3.forEach((h3Element) => {
-		h3Element.classList.remove('dark-mode-for-h3');
-		h3Element.classList.add('light-mode-for-h3');
+	headings.forEach((h3Element) => {
+		h3Element.classList.remove('dark-mode-for-heading');
+		h3Element.classList.add('light-mode-for-heading');
 	});
 }
 
@@ -41,9 +41,9 @@ modeButton.addEventListener("click", () => {
 			// cardElement.style.color = "#fff";
 			// cardElement.style.border = "solid 3px #e8c1c5";
 		});
-		h3.forEach((h3Element) => {
-			h3Element.classList.remove('light-mode-for-h3');
-			h3Element.classList.add('dark-mode-for-h3');
+		headings.forEach((h3Element) => {
+			h3Element.classList.remove('light-mode-for-heading');
+			h3Element.classList.add('dark-mode-for-heading');
 			// h3Element.style.color = "#fff";
 		});
 	} else {
@@ -58,9 +58,9 @@ modeButton.addEventListener("click", () => {
 			// cardElement.style.color = "#000";
 			// cardElement.style.border = "solid 3px #2e294e";
 		});
-		h3.forEach((h3Element) => {
-			h3Element.classList.remove('dark-mode-for-h3');
-			h3Element.classList.add('light-mode-for-h3');
+		headings.forEach((h3Element) => {
+			h3Element.classList.remove('dark-mode-for-heading');
+			h3Element.classList.add('light-mode-for-heading');
 			// h3Element.style.color = "#2e294e";
 		});
 	}
