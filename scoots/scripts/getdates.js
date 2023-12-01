6f57b1789ce65ed846c49252255bf9b6c3fc1e7e
coralin <zxc188950@yahoo.com.tw>
2023-12-01 11:39:36 +0800

    final

--- scoots/scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// 1
-const yearElement = document.querySelector('footer p');
-yearElement.innerHTML = `©${new Date().getFullYear()}<br/>Lin, Chih-Yun<br/>Taiwan`;
-// 2
-const lastModifiedElement = document.querySelector('#lastModified');
-let oLastModif = new Date(document.lastModified);
-lastModifiedElement.innerHTML = oLastModif
